365c3d06306e500e1a820cc43bc642c43dde8261
Remco <hello@dougley.com>
2019-02-03 19:45:49 +0100

    some misc changes

---
+++ README.md
@@ -0,0 +1,6 @@
+# DFeedbackPlus
+
+This is a webbrowser extension for extra functionality on [Discord's Feedback website](https://dis.gd/feedback)
+
+[![](https://developer.chrome.com/webstore/images/ChromeWebStore_BadgeWBorder_v2_206x58.png)](https://chrome.google.com/webstore/detail/kddnmabdnlffochhknicdaebhnhkedpf)
+[![](https://addons.cdn.mozilla.net/static/img/addons-buttons/AMO-button_1.png)](https://addons.mozilla.org/en/firefox/addon/dfeedbackplus/)
--- popup/js/popup.js
+++ popup/js/popup.js
@@ -1,9 +1,18 @@
 (() => {
   document
-  .getElementById('clickable')
+  .getElementById('veryLegitMenuOption')
   .addEventListener('click', function(e) {
     chrome.tabs.create({
         url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    });
+  window.close();
+  })
+  
+  document
+  .getElementById('sourceCodeOption')
+  .addEventListener('click', function(e) {
+    chrome.tabs.create({
+        url: 'https://github.com/Dougley/DFeedbackPlus'
     });
   window.close();
   })
